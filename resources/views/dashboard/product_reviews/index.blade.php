e4bd4e3941fd8782227534d939465f188704d4f1
irfanarfianto <irfanarfianto92@gmail.com>
2024-06-26 23:55:23 +0700

    chore: Update padding classes in index.blade.php for consistency

--- resources/views/dashboard/product_reviews/index.blade.php
+++ resources/views/dashboard/product_reviews/index.blade.php
@@ -11,16 +11,53 @@
             <details class="dropdown dropdown-end">
                 <summary class="btn m-1">{{ __('Urutkan') }}</summary>
                 <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-                    <li><a href="{{ route('reviews.index', ['sort_by' => 'latest']) }}">{{ __('Terbaru') }}</a></li>
-                    <li><a href="{{ route('reviews.index', ['sort_by' => 'oldest']) }}">{{ __('Terlama') }}</a></li>
-                    <li><a
-                            href="{{ route('reviews.index', ['sort_by' => 'highest_rating']) }}">{{ __('Rating Tertinggi') }}</a>
+                    <li>
+                        <a href="{{ route('reviews.index', ['sort_by' => 'latest']) }}" class="flex items-center justify-between">
+                            {{ __('Terbaru') }}
+                            @if (request()->get('sort_by') == 'latest')
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                                </svg>
+                            @endif
+                        </a>
                     </li>
-                    <li><a
-                            href="{{ route('reviews.index', ['sort_by' => 'lowest_rating']) }}">{{ __('Rating Terendah') }}</a>
+                    <li>
+                        <a href="{{ route('reviews.index', ['sort_by' => 'oldest']) }}" class="flex items-center justify-between">
+                            {{ __('Terlama') }}
+                            @if (request()->get('sort_by') == 'oldest')
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                                </svg>
+                            @endif
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('reviews.index', ['sort_by' => 'highest_rating']) }}" class="flex items-center justify-between">
+                            {{ __('Rating Tertinggi') }}
+                            @if (request()->get('sort_by') == 'highest_rating')
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                                </svg>
+                            @endif
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('reviews.index', ['sort_by' => 'lowest_rating']) }}" class="flex items-center justify-between">
+                            {{ __('Rating Terendah') }}
+                            @if (request()->get('sort_by') == 'lowest_rating')
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                                </svg>
+                            @endif
+                        </a>
                     </li>
                 </ul>
             </details>
+
         </div>
     </div>
     <div class="container mx-auto">
