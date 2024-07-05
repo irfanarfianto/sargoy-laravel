1a7b30b5cf2353f05075ed3166b5fe0e1db1c65d
irfanarfianto <irfanarfianto92@gmail.com>
2024-07-05 18:43:29 +0700

    Update language files for pagination, authentication, and password reset
    This commit updates the language files for pagination, authentication, and password reset in the Indonesian language. The changes include translating the pagination links, error messages, and success messages to better match the application's requirements.

---
+++ lang/id/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Email atau kata sandi yang Anda masukkan tidak cocok.',
+    'password' => 'Kata sandi yang Anda masukkan salah.',
+    'throttle' => 'Terlalu banyak percobaan masuk. Silakan coba lagi dalam :seconds detik.',
+
+
+];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,4 @@
 <x-guest-layout>
-
     <h1 class="text-3xl font-bold text-center mb-2">{{ __('Daftar Akun') }}</h1>
     <p class="text-sm text-center text-gray-600 mb-4">
         Selamat datang di Official Store Sarung Goyor.
@@ -12,7 +11,7 @@
         <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
-                autofocus autocomplete="name" />
+                autofocus autocomplete="name" placeholder="Masukkan Nama Anda" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
@@ -20,27 +19,24 @@
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                required autocomplete="email" />
+                required autocomplete="email" placeholder="Masukkan Email Anda" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                autocomplete="new-password" />
-
+                autocomplete="new-password" placeholder="Masukkan Password Anda" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
             <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                name="password_confirmation" required autocomplete="new-password" />
-
+                name="password_confirmation" required autocomplete="new-password"
+                placeholder="Konfirmasi Password Anda" />
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
