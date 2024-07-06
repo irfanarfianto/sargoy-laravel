5149c3dae0b9a41218a881c71f6b3b910a47c843
irfanarfianto <irfanarfianto92@gmail.com>
2024-07-06 21:24:11 +0700

    Refactor menu-item component and add badge support
    This commit refactors the menu-item component by adding support for displaying a badge. The badge is only shown if the user is an admin. This change improves the flexibility and functionality of the menu-item component.

--- resources/views/layouts/drawer-side.blade.php
+++ resources/views/layouts/drawer-side.blade.php
@@ -1,20 +1,18 @@
 <div class="drawer-side">
     <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
     <ul class="menu p-4 w-72 min-h-full bg-blue-950 text-base-100 fixed mt-16">
-        <!-- Sidebar content here -->
         <!-- Dashboard Section -->
-        <li class="menu-title text-gray-500">
+        <p class="menu-title text-gray-500">
             <span>Dashboard</span>
-        </li>
+        </p>
         @if (auth()->user()->hasRole('seller'))
             <li>
                 <a href="{{ route('seller') }}" class="{{ request()->routeIs('seller') ? 'text-indigo-500' : '' }}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
+                            d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
+
                     </svg>
                     Dashboard
                 </a>
@@ -26,9 +24,8 @@
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
+                            d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
+
                     </svg>
                     Dashboard
                 </a>
@@ -36,9 +33,9 @@
         @endif
 
         <!-- Product Section -->
-        <li class="menu-title text-gray-500">
+        <p class="menu-title text-gray-500">
             <span>Produk</span>
-        </li>
+        </p>
         <li>
             <a href="{{ route('dashboard.product.index') }}"
                 class="{{ request()->routeIs('dashboard.product.index', 'dashboard.product.tambah', 'dashboard.product.edit') ? 'text-indigo-500' : '' }}">
@@ -47,12 +44,26 @@
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                 </svg>
-                Daftar Produk
+                List Produk
                 @if (isset($unverifiedProductCount) && auth()->user()->hasRole('admin'))
                     <span class="badge badge-secondary">{{ $unverifiedProductCount }}</span>
                 @endif
             </a>
         </li>
+        @if (auth()->user()->hasRole('admin'))
+            <li>
+                <a href="{{ route('categories.index') }}"
+                    class="{{ request()->routeIs('categories.index', 'categories.create', 'categories.edit') ? 'text-indigo-500' : '' }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
+                    </svg>
+                    Kategori
+                </a>
+            </li>
+        @endif
         <li>
             <a href="{{ route('reviews.index') }}"
                 class="{{ request()->routeIs('reviews.index', 'reviews.create', 'reviews.edit') ? 'text-indigo-500' : '' }}">
@@ -68,27 +79,11 @@
             </a>
         </li>
 
-
-        @if (auth()->user()->hasRole('admin'))
-            <li>
-                <a href="{{ route('categories.index') }}"
-                    class="{{ request()->routeIs('categories.index', 'categories.create', 'categories.edit') ? 'text-indigo-500' : '' }}">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
-                    </svg>
-                    Kategori
-                </a>
-            </li>
-        @endif
-
         <!-- Users Section -->
         @if (auth()->user()->hasRole('admin'))
-            <li class="menu-title text-gray-500">
-                <span>Pengguna</span>
-            </li>
+            <p class="menu-title text-gray-500">
+                <span>Manajemen Pengguna</span>
+            </p>
             <li>
                 <a href="{{ route('users.index') }}"
                     class="{{ request()->routeIs('users.index', 'users.create', 'users.edit') ? 'text-indigo-500' : '' }}">
@@ -113,9 +108,9 @@
         @endif
 
         <!-- FAQ Section -->
-        <li class="menu-title text-gray-500">
+        <p class="menu-title text-gray-500">
             <span>Lain-lain</span>
-        </li>
+        </p>
         <li>
             <a href="{{ route('pesan') }}" class="{{ request()->routeIs('pesan') ? 'text-indigo-500' : '' }}">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
