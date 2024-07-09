791acb5b7529685b71edce1628ed31a21c934fbb
irfanarfianto <irfanarfianto92@gmail.com>
2024-07-09 21:22:06 +0700

    refactor: Improve table layout in blogs index page
    The code changes in index.blade.php refactor the table layout in the blogs index page. The changes include wrapping the table in a div with horizontal scrolling, improving the visual presentation of the table. This enhancement provides a better user experience when viewing and interacting with the blog posts table.

--- resources/views/dashboard/blogs/index.blade.php
+++ resources/views/dashboard/blogs/index.blade.php
@@ -17,61 +17,59 @@
             </div>
         @endif
 
-        <table class="table table-zebra min-w-full bg-white shadow-md rounded-lg overflow-hidden">
-            <thead class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                <tr>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Created At</th>
-                    <th>Status</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($posts as $post)
+        <div class="overflow-x-auto">
+            <table class="table table-zebra min-w-full bg-white shadow-md rounded-lg overflow-hidden">
+                <thead class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
                     <tr>
-                        <td>
-                            <div class="flex flex-col gap-1">
-                                {{ Str::limit($post->title, 50) }}
-                                <div class="flex flex-warp">
-                                    @if ($post->tags)
-                                        @php
-                                            $tags = json_decode($post->tags);
-                                        @endphp
-
-                                        @foreach ($tags as $tag)
-                                            <span class="badge text-xs mr-1">#{{ $tag }}</span>
-                                        @endforeach
-                                    @endif
-                                </div>
-                            </div>
-                        </td>
-                        <td>{{ $post->author }}</td>
-                        <td>{{ $post->created_at->format('d M Y') }}</td>
-                        <td>
-                            @if ($post->recommended)
-                                <span>
-                                    <span class="badge badge-success badge-outline badge-sm">Rekomendasi</span>
-                                </span>
-                            @else
-                                <span>
-                                    <span class="badge badge-error badge-outline badge-sm">Tidak Rekomendasi</span>
-                                </span>
-                            @endif
-                        </td>
-                        <td>
-                            <a class="btn btn-ghost btn-xs"
-                                onclick="document.getElementById('detailModal{{ $post->id }}').showModal()">Detail</a>
-                            <a href="{{ route('blogs.edit', $post->slug) }}" class="btn btn-ghost btn-xs">Edit</a>
-                            <a class="btn btn-error btn-xs"
-                                onclick="document.getElementById('delete{{ $post->id }}').showModal()">Hapus</a>
-                        </td>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Created At</th>
+                        <th>Status</th>
+                        <th>Actions</th>
                     </tr>
-                    @include('dashboard.blogs.partials.hapus')
-                    @include('dashboard.blogs.partials.detail')
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td>
+                                <div class="flex flex-col gap-1">
+                                    {{ Str::limit($post->title, 50) }}
+                                    <div class="flex flex-warp">
+                                        @if ($post->tags)
+                                            @php
+                                                $tags = json_decode($post->tags);
+                                            @endphp
+
+                                            @foreach ($tags as $tag)
+                                                <span class="badge text-xs mr-1">#{{ $tag }}</span>
+                                            @endforeach
+                                        @endif
+                                    </div>
+                                </div>
+                            </td>
+                            <td>{{ $post->author }}</td>
+                            <td>{{ $post->created_at->format('d M Y') }}</td>
+                            <td>
+                                @if ($post->recommended)
+                                    <span>
+                                        <span class="badge badge-success badge-outline badge-sm">Rekomendasi</span>
+                                    </span>
+                                @endif
+                            </td>
+                            <td>
+                                <a class="btn btn-ghost btn-xs"
+                                    onclick="document.getElementById('detailModal{{ $post->id }}').showModal()">Detail</a>
+                                <a href="{{ route('blogs.edit', $post->slug) }}" class="btn btn-ghost btn-xs">Edit</a>
+                                <a class="btn btn-error btn-xs"
+                                    onclick="document.getElementById('delete{{ $post->id }}').showModal()">Hapus</a>
+                            </td>
+                        </tr>
+                        @include('dashboard.blogs.partials.hapus')
+                        @include('dashboard.blogs.partials.detail')
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
 
         <div class="mt-4">
             {{ $posts->links('vendor.pagination.custom') }}
