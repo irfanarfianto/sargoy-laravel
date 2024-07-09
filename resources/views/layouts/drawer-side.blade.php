b12abe15f89a6906fda515ce94ff5f51c9809e79
irfanarfianto <irfanarfianto92@gmail.com>
2024-07-09 12:19:59 +0700

    feat: Update navigation menu with links to about, products, and blogs pages

--- resources/views/layouts/drawer-side.blade.php
+++ resources/views/layouts/drawer-side.blade.php
@@ -165,7 +165,7 @@
         </li>
         <li>
             <button onclick="document.getElementById('keluar').showModal()"
-                class="text-white bg-red-600 hover:bg-red-700 px-4 py-2 rounded">
+                class="text-white hover:bg-red-700 px-4 py-2 rounded">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
