64366408312b762c72c32703ea39c5fca71e7427
irfanarfianto <irfanarfianto92@gmail.com>
2024-07-11 23:29:57 +0700

    feat: Implementasi PWA Laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link rel="icon" href="{{ asset('/logo.png') }}" type="image/png">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    @laravelPWA
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -138,7 +138,7 @@
 
                                     @for ($i = 0; $i < $filledStars; $i++)
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                            fill="currentColor" class="size-6 text-orange-400">
+                                            fill="currentColor" class="size-4 text-orange-400">
                                             <path fill-rule="evenodd"
                                                 d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
                                                 clip-rule="evenodd" />
@@ -147,7 +147,7 @@
 
                                     @for ($i = 0; $i < $emptyStars; $i++)
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                            fill="currentColor" class="size-6 text-gray-300">
+                                            fill="currentColor" class="size-4 text-gray-300">
                                             <path fill-rule="evenodd"
                                                 d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
                                                 clip-rule="evenodd" />
