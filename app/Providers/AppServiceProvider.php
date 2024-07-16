dd24da53a803085bf8bffb62e6f07ebb647c1317
irfanarfianto <irfanarfianto92@gmail.com>
2024-07-16 17:25:03 +0700

    refactor: Update URL scheme to force HTTPS in production environment
    Update the AppServiceProvider.php file to change the URL scheme to force HTTPS in the production environment. This change ensures that the application always uses a secure connection for improved security and data protection.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') != 'local') {
+        if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
 
