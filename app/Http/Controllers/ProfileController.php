5812972caf8544218c6163f8fc570f55046bc933
irfanarfianto <irfanarfianto92@gmail.com>
2024-07-18 16:37:34 +0700

    refactor profilecontroller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -52,12 +52,12 @@ class ProfileController extends Controller
     /**
      * Display the user's profile form.
      */
-    public function edit(Request $request): View
-    {
-        return view('profile.edit', [
-            'user' => $request->user(),
-        ]);
-    }
+    // public function edit(Request $request): View
+    // {
+    //     return view('profile.edit', [
+    //         'user' => $request->user(),
+    //     ]);
+    // }
 
     /**
      * Update the user's profile information.
--- routes/web/view.php
+++ routes/web/view.php
@@ -42,7 +42,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::prefix('dashboard')->group(function () {
 
         Route::get('profile', [ProfileController::class, 'index'])->name('profile.index');
-        Route::get('profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+        // Route::get('profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
         Route::middleware('role:admin')->group(function () {
