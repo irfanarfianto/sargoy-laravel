9948b9bbcab53b2e1589f628bb9a0541f6abc4cd
irfanarfianto <irfanarfianto92@gmail.com>
2024-07-19 16:20:36 +0700

    refactor: Fix registration success flash message and remove commented code

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,7 +31,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
@@ -43,11 +43,8 @@ class RegisteredUserController extends Controller
         $user->assignRole('visitor');
         event(new Registered($user));
 
-        // Send verification email
-        // $user->sendEmailVerificationNotification();
+        flash()->success('Registrasi berhasil, silahkan login.');
 
-        // return redirect()->route('verification.notice');
-        
         return redirect()->route('login');
     }
 }
